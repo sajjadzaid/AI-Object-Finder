d103e245614f60fc42faccc321a9d1922ffcb71c
sajjadzaid <92423630+sajjadzaid@users.noreply.github.com>
2022-06-28 20:04:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+function setup(){
+    canvas=createCanvas(480,380)
+    canvas.center();
+
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status : Detecting Objects";
+    object_name= document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("CocoSSD has been initialized");
+    status= true;
+    
+}
+
+
+
+
+
+
